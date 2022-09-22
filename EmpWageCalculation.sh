6eb7b3dd5b9ed00f9e55a8ef4ec39f0402cad041
Soumya-Ranjan-Singh <soumyars675@gmail.com>
2022-09-22 09:35:55 +0530

    Added in : UC 2- Calculate Daily Employee Wage

--- EmpWageCalculation.sh
+++ EmpWageCalculation.sh
@@ -5,7 +5,11 @@ randomCheck=$((RANDOM%2))
 
 if [ $isPresent -eq $randomCheck ];
 then
-	echo "Employee is Present"
+	empRatePerHr=20;
+	empHrs=8;
+	salary=$(($empHrs*$empRatePerHr))
+	echo "salary=$salary"
 else
-	echo "Employee is Absent"
+	salary=0
+	echo "salary=$salary"
 fi
